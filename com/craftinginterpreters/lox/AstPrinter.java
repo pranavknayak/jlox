f0c3e68bb9722ab2c343e3881ea73c173e560b64
Pranav K Nayak <es20btech11035@iith.ac.in>
2022-12-22 10:42:07 +0530

    finished ast printing

---
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -0,0 +1,41 @@
+package com.craftinginterpreters.lox;
+
+public class AstPrinter implements Expr.Visitor<String> {
+  String print(Expr expr) {
+    return expr.accept(this);
+  }
+
+  @Override
+  public String visitBinaryExpr(Expr.Binary expr) {
+    return paranthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  @Override
+  public String visitGroupingExpr(Expr.Grouping expr) {
+    return paranthesize("group", expr.expression);
+  }
+
+  @Override
+  public String visitLiteralExpr(Expr.Literal expr) {
+    if (expr.value == null)
+      return "nil";
+    return expr.value.toString();
+  }
+
+  @Override
+  public String visitUnaryExpr(Expr.Unary expr) {
+    return paranthesize(expr.operator.lexeme, expr.right);
+  }
+
+  private String paranthesize(String name, Expr... exprs) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("(").append(name);
+    for (Expr expr : exprs) {
+      builder.append(" ");
+      builder.append(expr.accept(this));
+    }
+    builder.append(")");
+
+    return builder.toString();
+  }
+}
